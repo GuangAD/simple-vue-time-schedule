8fb46c4740f84269682568a2f0569b465d4a5ad1
tim <1536576466@qq.com>
2025-11-23 19:56:34 +0800

    refactor: 更新依赖项并调整导入路径
    将本地模块引用改为npm包形式，添加样式文件导入
    移除未使用的依赖项debounce和fastest-json-copy

--- playground/main.ts
+++ playground/main.ts
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import { TimeSchedulePlugin } from '../src/index.ts'
+
+import { TimeSchedulePlugin } from '@ggc12319/simple-vue-time-schedule'
+import '@ggc12319/simple-vue-time-schedule/style.css'
+
 createApp(App).use(TimeSchedulePlugin).mount('#app')
